76ce8c4de88430df4fd4910b2c36293d590a8556
99mini <itman390@gmail.com>
2023-12-16 22:23:11 +0900

    a

--- scripts/update_package_version.sh
+++ scripts/update_package_version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 version=${1:-'patch'}
-NEW_VERSION=$(npm --no-git-tag-version version $version) && git commit -am "Update Version to v$NEW_VERSION"
+NEW_VERSION=$(npm --no-git-tag-version version $version) && git commit -am "Update Version to $NEW_VERSION"
